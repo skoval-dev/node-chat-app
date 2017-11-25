934e2f2fc49898e37b2f3f434ac8e7a63ca37855
Serhii Koval <dev.skoval@gmail.com>
2017-11-25 17:02:10 +0200

    Add connect and disconect listeners

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+let socket = io();
+socket.on("connect", () => {
+   console.log("Connected to server!");
+});
+
+socket.on("disconnect", () => {
+   console.log("Disconnected from server!")
+});
--- server/server.js
+++ server/server.js
@@ -1,12 +1,27 @@
 const path = require("path");
+const http = require("http");
 const public_path = path.join(__dirname, "..", "/public");
 const express = require("express");
 const port = process.env.PORT || 3000;
+const socket_io = require("socket.io");
 
-const app = express();
+let app = express();
+let server = http.createServer(app);
+let io = socket_io(server);
+
 app.use(express.static(public_path));
 
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+   console.log("New user connected");
+
+
+   socket.on("disconnect", () => {
+      console.log("User was disconnected!");
+   });
+});
+
+
+server.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
 });
